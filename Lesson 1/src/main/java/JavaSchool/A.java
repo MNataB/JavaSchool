225b7db58ae7457827247e3df83412c22a37bb73
MNataB <n.b.manuilova@gmail.com>
2017-03-17 22:19:41 +0300

    test

--- Lesson 1/src/main/java/JavaSchool/A.java
+++ Lesson 1/src/main/java/JavaSchool/A.java
@@ -1,8 +1,12 @@
+package JavaSchool;
+
+import JavaSchool.Utils.B;
+
 /**
  * Created by Наталья on 16.03.2017.
  */
 public class A {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        new B().out();
     }
 }
---
+++ Lesson 1/src/main/java/JavaSchool/Utils/B.java
@@ -0,0 +1,10 @@
+package JavaSchool.Utils;
+
+/**
+ * Created by Наталья on 16.03.2017.
+ */
+public class B {
+    public void out () {
+        System.out.println("I'm here - in B");
+    }
+}

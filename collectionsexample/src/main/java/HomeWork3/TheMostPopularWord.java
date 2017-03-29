d60ce3f79929f03c7374962dd1650dca4e09f1a6
MNataB <n.b.manuilova@gmail.com>
2017-03-30 02:31:50 +0300

    Remove directory 1

---
+++ collectionsexample/src/main/java/HomeWork3/TheMostPopularWord.java
@@ -0,0 +1,68 @@
+package HomeWork3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import static HomeWork3.ReadFile.loadWords;
+
+/**
+ * Created by Наталья on 29.03.2017.
+ */
+class ReadFile {
+    static List<String> loadWords (FileReader in) throws IOException {
+        BufferedReader reader = new BufferedReader(in);
+        List<String> lines = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty())
+                continue;
+            lines.add(line);
+        }
+        reader.close();
+        return lines;
+    }
+}
+
+public class TheMostPopularWord {
+    public static void main (String[] args) throws IOException {
+        FileReader in = new FileReader("input.txt");
+        List<String> lines = loadWords(in);
+        ArrayList<String> words = new ArrayList<>();
+        for (String list : lines) {
+            String[] strings = list.split(" ");
+            for (String string : strings) {
+                words.add(string.toLowerCase());
+            }
+        }
+
+        Map<String, Integer> counterWords = new HashMap<>();
+        Integer maxCount = 1;
+        Integer minCount = 1;
+        for (String word : words) {
+            if(!word.isEmpty()) {
+                Integer count = counterWords.get(word);
+                if(count == null) {
+                    count = 0;
+                }
+                counterWords.put(word, ++count);
+                if (counterWords.get(word) > maxCount) {
+                    maxCount = counterWords.get(word);
+                }
+            }
+        }
+
+        if (maxCount == minCount) minCount = 0;
+        Map<String, Integer> maxWords = new TreeMap<>();
+        for(String word : counterWords.keySet()) {
+            if (counterWords.get(word) != minCount) {
+                maxWords.put(word, counterWords.get(word));
+            }
+        }
+
+        for(String word : maxWords.keySet()) {
+            System.out.println(word);
+        }
+    }
+}

fa88ea2b2c1b100fadff79fa7ac319ead0284781
MNataB <n.b.manuilova@gmail.com>
2017-03-30 14:36:07 +0300

    The first load

---
+++ collectionsexample/src/main/java/HomeWork3/SetOfNumbers.java
@@ -0,0 +1,35 @@
+package HomeWork3;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by sbt-manuilova-nb on 30.03.2017.
+ */
+public class SetOfNumbers {
+    public static void main(String[] args) throws IOException {
+        FileReader in = new FileReader("input.txt");
+        List<String> lines = ReadFile.loadLines(in);
+        List<Integer> numbers = new ArrayList<>();
+        Integer countOper = Integer.valueOf(lines.get(0));
+        String firstValue;
+        String secondValue;
+
+        for (int i = 1; i <= countOper; i++) {
+            firstValue = lines.get(i).length() == 1 ? lines.get(i) : lines.get(i).substring(0, lines.get(i).indexOf(" "));
+            secondValue = lines.get(i).length() == 1 ? null : lines.get(i).substring(lines.get(i).indexOf(" ") + 1);
+            if (firstValue.equals("1")) {
+                numbers.add(Integer.valueOf(secondValue));
+                Collections.sort(numbers);
+            }
+            else {
+                System.out.println(numbers.get(0));
+                numbers.remove(numbers.get(0));
+            }
+        }
+    }
+}
---
+++ collectionsexample/src/main/java/Trash/Test1.java
@@ -0,0 +1,12 @@
+package Trash;
+
+/**
+ * Created by sbt-manuilova-nb on 30.03.2017.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        Integer count = 0;
+        System.out.println(++count);
+        System.out.println(count);
+    }
+}
